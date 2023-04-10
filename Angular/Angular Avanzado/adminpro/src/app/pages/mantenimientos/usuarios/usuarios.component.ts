364ebe3f2271c345ac90b91b58d1be3775945166
Geronimo Jauregui <geronimo.jauregui@isita.com.mx>
2023-04-10 15:33:49 -0600

    Add: Se implementa la búsqueda de usuarios

--- Angular/Angular Avanzado/adminpro/src/app/pages/mantenimientos/usuarios/usuarios.component.ts
+++ Angular/Angular Avanzado/adminpro/src/app/pages/mantenimientos/usuarios/usuarios.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/models/usuario.model';
+import { BusquedasService } from 'src/app/services/busquedas.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
@@ -11,9 +12,11 @@ import { UsuarioService } from 'src/app/services/usuario.service';
 export class UsuariosComponent implements OnInit {
   public totalUsuarios: number = 0;
   public usuarios: Usuario[] = [];
+  public usuariosTemp: Usuario[] = [];
   public desde: number = 0;
   public cargando: boolean = true;
-  constructor(private usuarioServices: UsuarioService) { }
+  constructor(private usuarioServices: UsuarioService,
+              private busquedasService: BusquedasService) { }
 
   ngOnInit(): void {
     this.cargarUsuarios();
@@ -24,6 +27,7 @@ export class UsuariosComponent implements OnInit {
     this.usuarioServices.cargarUsuarios(this.desde).subscribe( (resp: any) => {
       this.totalUsuarios = resp.total;
       this.usuarios = resp.usuarios;
+      this.usuariosTemp = resp.usuarios;
       this.cargando = false;
     })
   }
@@ -36,6 +40,16 @@ export class UsuariosComponent implements OnInit {
       this.desde -= valor;
     }
     this.cargarUsuarios();
+  }
+
+  buscar(termino: string){
+    if( termino.length === 0){
+      this.usuarios = this.usuariosTemp;
+    } else {
+      this.busquedasService.buscar('usuarios', termino).subscribe( (resp: any) => {
+        this.usuarios = resp;
+      });
+    }
   }
 
 }
---
+++ Angular/Angular Avanzado/adminpro/src/app/services/busquedas.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Usuario } from '../models/usuario.model';
+const base_url = environment.base_url;
+@Injectable({
+  providedIn: 'root'
+})
+export class BusquedasService {
+
+  constructor(private http: HttpClient) { }
+
+  get token(): string {
+    return localStorage.getItem('token') || '';
+  }
+
+  get headers() {
+    return {
+      headers: {
+        'x-token': this.token
+      }
+    }
+  }
+  private transformarUsuarios(resultado: any[]): Usuario[] {
+    return resultado.map(
+      user => new Usuario(user.nombre, user.email, user.uid, '', user.img, user.role, user.google)
+    );
+  }
+  buscar(tipo: 'usuarios'|'medicos'|'hospitales', termino: string){
+    return this.http.get<any[]>(`${base_url}/todo/coleccion/${tipo}/${termino}`,this.headers)
+    .pipe(
+      map( (resp: any) => {
+        switch(tipo) {
+          case 'usuarios':
+            return this.transformarUsuarios(resp.resultado);
+          break;
+
+          default:
+            return [];
+        }
+      })
+    );
+  }
+}
